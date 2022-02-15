d3379a8ed988530524faa48a629f3d92d38d7763
simoncreek <simoncreek@tutanota.com>
2022-02-16 02:06:49 +0900

    Initial commit

---
+++ docs/meta.md
@@ -0,0 +1,7 @@
+# TODO
+
+- Set up discord application (Login, get token, ETC)
+- Learn the basics of luvit framework
+- Learn the basics of discordia
+- Connect gcalc api
+- Create discord bot
---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "lgcalc",
+    version = "0.0.1",
+    description = "Discord gcalc bot made with lua",
+    tags = { "lua", "discord", "bot", "gcalc", "game" },
+    license = "MIT",
+    author = { name = "simoncreek", email = "simoncreek@tutanota.com" },
+    homepage = "https://github.com/simhyeon/lgcalc",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
