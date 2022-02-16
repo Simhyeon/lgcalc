7f2e13ce9b0e8a572cad505ad66e96559642196d
simoncreek <simoncreek@tutanota.com>
2022-02-16 23:07:30 +0900

    2000 text limit compatible change

--- bot.lua
+++ bot.lua
@@ -1,6 +1,8 @@
 require 'src/util'
 require 'src/gcalc'
 
+local SEND_LIMIT = 2000
+
 -- This is all sample text from discorida github
 local discordia = require('discordia')
 local client = discordia.Client()
@@ -18,8 +20,42 @@ client:on('messageCreate', function(message)
 			data = "ERR : Invalid calculation option\n" .. data
 		end
 
-		-- Send result wrapped in code block for proper formatting
-		message.channel:send("```\n" .. data .. "\n```")
+		local split = split_string(data,"\n")
+
+		-- Loop local variables to avoid assignment 
+		-- START
+		local chunk = "```\n"
+		local separator = "\n```"
+		local next_chunk = ""
+		local counter = 1
+		local target_line = ""
+		local line_sep = "\n"
+		-- END
+
+		while split[counter] do
+			target_line = split[counter]
+			next_chunk = chunk .. target_line
+
+			if split[counter+1] then
+				line_sep = "\n"
+			else
+				line_sep = ""
+			end
+
+			-- Aggregate chunk by lines for better visibility
+			if next_chunk:len() < SEND_LIMIT - separator:len() then
+				chunk = next_chunk .. line_sep -- Update chunk
+			else -- Split text if text is bigger than SEND_LIMIT
+				print(chunk .. separator) -- Send chunk
+				chunk = "```\n" .. target_line .. line_sep
+			end
+			counter = counter + 1
+		end
+
+		-- Flush remaining chunk
+		if chunk:len() ~= 0 then
+			print(chunk .. separator) -- Send chunk
+		end
 	end
 end)
 
